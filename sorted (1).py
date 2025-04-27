b3c67b141d3b25b91c1b91dc21a30ed5523f0f43
pranavgit52 <chaudharipranav869@gmail.com>
2025-04-27 11:39:07 +0530

    Update sorted (1).py

--- sorted (1).py
+++ sorted (1).py
@@ -1,23 +1,18 @@
 def selection_sort_greedy(arr):
-    n = len(arr)  
-    print("\nList before Sorting: ", arr, "\n")  
-
+    n = len(arr)
+    print("\nList before Sorting: ", arr,"\n")
     for i in range(n):
-        min_idx = i  
-        for j in range(i+1, n):  
+        min_idx = i
+        for j in range(i+1, n):
             if arr[j] < arr[min_idx]:
-                min_idx = j  
-        
+                min_idx = j
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        
-        print("List After Pass", i+1, ":", arr)  
+        print("List After Pass ",i+1,": ",arr)
+    return arr
 
-    return arr 
-
-n = int(input("Length of List: "))
-arr = []
+n=int(input("Length of List: "))
+arr=[]
 for i in range(n):
-    element = int(input("Enter List Element: "))
+    element=int(input("Enter List Element: "))
     arr.append(element)
-s = selection_sort_greedy(arr)
-print("\nSorted List is:", s)
+print("\nSorted List is:", selection_sort_greedy(arr))
